6234a7d7c2b7565551c3e6cc92b757ef0daeed44
ABozzelli <Andrea.Bozzelli@siav.it>
2021-06-20 18:52:39 +0200

    test setup

---
+++ src/test/java/controller/EmailControllerTest.java
@@ -0,0 +1,31 @@
+package controller;
+
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+public class EmailControllerTest {
+
+    @Test
+    @Category(controller.EmailController.class)
+    public void test_get_new_mailbox() {
+
+    }
+
+    @Test
+    @Category(controller.EmailController.class)
+    public void test_get_all_mail_of_mailbox() {
+
+    }
+
+    @Test
+    @Category(controller.EmailController.class)
+    public void test_get_mail() {
+
+    }
+
+    @Test
+    @Category(controller.EmailController.class)
+    public void test_get_attachment() {
+
+    }
+}
